4466b6b6672d2fab9642b5958c8ec573e884cd18
leepachia <pachia0lee@gmail.com>
2024-10-14 02:32:20 -0500

    Added statistics screen
    Still need to add a box plot or some kind of chart.

---
+++ Pages/Statistics.xaml.cs
@@ -0,0 +1,16 @@
+namespace QuizzingApp341.Pages;
+
+public partial class Statistics : ContentPage
+{
+	public Statistics()
+	{
+		InitializeComponent();
+	}
+
+    // Event handler for button click
+    private void ViewAllParticipantsClicked(object sender, EventArgs e)
+    {
+        // Get the participants and show the participants screen
+
+    }
+}
