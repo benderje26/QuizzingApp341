024b8bb0fc73eb10655d92f966e7c67d19120df3
Peter Skogman <skogmanpe40@uwosh.edu>
2024-10-13 20:36:15 -0500

    multiple choice and fill in blank screens

---
+++ Pages/FillBlank.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.Maui.Controls.PlatformConfiguration;
+
+namespace QuizzingApp341.Pages;
+
+public partial class FillBlank : ContentPage
+{
+	public FillBlank()
+	{
+		InitializeComponent();
+	}
+}
