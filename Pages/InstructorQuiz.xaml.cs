58c31aa208d18c9fcf630b6f96f9f6e2e1fc8dba
leepachia <pachia0lee@gmail.com>
2024-10-13 21:43:38 -0500

    Added Instructor's Quiz Screen

---
+++ Pages/InstructorQuiz.xaml.cs
@@ -0,0 +1,18 @@
+namespace QuizzingApp341.Pages;
+
+public partial class InstructorQuiz : ContentPage
+{
+    public InstructorQuiz()
+    {
+        InitializeComponent();
+    }
+
+    // Event handler for button click
+    private void NextQuestionClicked(object sender, EventArgs e)
+    {
+        // Get the selected answers and process them
+        // This is where you handle the logic of what happens when the button is clicked
+
+        // For example, display an alert when clicked
+    }
+}
