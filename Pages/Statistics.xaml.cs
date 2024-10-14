47896509c8bbb2a80f63fc8f873e1b5cfd72d0c3
leepachia <pachia0lee@gmail.com>
2024-10-14 13:50:02 -0500

    Added img for box plot

--- Pages/Statistics.xaml.cs
+++ Pages/Statistics.xaml.cs
@@ -2,15 +2,16 @@ namespace QuizzingApp341.Pages;
 
 public partial class Statistics : ContentPage
 {
-	public Statistics()
-	{
-		InitializeComponent();
-	}
+    public Statistics()
+    {
+        InitializeComponent();
+    }
 
     // Event handler for button click
     private void ViewAllParticipantsClicked(object sender, EventArgs e)
     {
         // Get the participants and show the participants screen
-
     }
 }
+
+
