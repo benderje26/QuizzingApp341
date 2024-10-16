765126064507a0b57d29adccce9fbb3613109f84
jbender3 <benderje26@uwosh.edu>
2024-10-16 18:18:21 -0500

    Add name to files

--- Pages/CreateAccount.xaml.cs
+++ Pages/CreateAccount.xaml.cs
@@ -1,5 +1,8 @@
 namespace QuizzingApp341.Pages;
 
+/*
+ * Name: Jeremiah Bender
+ */
 public partial class CreateAccount : ContentPage
 {
 	public CreateAccount()
--- Pages/ResetPassword.xaml.cs
+++ Pages/ResetPassword.xaml.cs
@@ -1,5 +1,8 @@
 namespace QuizzingApp341.Pages;
 
+/*
+ * Name: Jeremiah Bender
+ */
 public partial class ResetPassword : ContentPage
 {
 	public ResetPassword()
