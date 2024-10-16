6ff1a773ad4a791f22b55f95e073c6840fe68dd4
wangj97 <wangj97@uwosh.edu>
2024-10-16 09:57:08 -0500

    add CreateQuiz

---
+++ Pages/CreateQuiz.xaml.cs
@@ -0,0 +1,23 @@
+using Microsoft.Maui.Controls.PlatformConfiguration;
+
+namespace QuizzingApp341.Pages;
+
+public partial class CreateQuiz : ContentPage
+{
+    public CreateQuiz()
+    {
+        InitializeComponent();
+    }
+
+
+    // Event handler for button click
+    private void StudyButtonClickedCreateQuiz(object sender, EventArgs e){
+        // pull out the quize that got clicked
+     }
+
+
+     private void OnAddButtonClicked(object sender, EventArgs e){
+                // Navigate to the page where users can create a new quiz
+     }
+
+}
