9a930cff2231af7b1a84acc42ab49e146daa7eb2
Zafeer Rahim <rahimza88@uwosh.edu>
2024-10-16 15:41:23 -0500

    Home Screen and search button working

--- Pages/Search.xaml.cs
+++ Pages/Search.xaml.cs
@@ -1,36 +1,35 @@
 using System.Collections.ObjectModel;
 using Microsoft.Maui.Controls;
 
-namespace QuizzingApp341.Pages
+namespace QuizzingApp341.Pages;
+
+public partial class Search : ContentPage
 {
-    public partial class SearchResultsPage : ContentPage
+    public ObservableCollection<QuizSearch> Quizzes { get; set; }
+
+    public Search()
     {
-        public ObservableCollection<Quiz> Quizzes { get; set; }
-
-        public SearchResultsPage()
+        InitializeComponent();
+        Quizzes = new ObservableCollection<QuizSearch>
         {
-            InitializeComponent();
-            Quizzes = new ObservableCollection<Quiz>
-            {
-                new Quiz { Title = "Anatomy Quiz 1", Creator = "Created by user1" },
-                new Quiz { Title = "Anatomy Quiz 2", Creator = "Created by user2" }
-            };
+            new QuizSearch ( "Anatomy Quiz 1",  "Created by user1" ),
+            new QuizSearch (  "Anatomy Quiz 2",  "Created by user2" )
+        };
 
-            BindingContext = this;
-        }
-
-        // Command for study button
-        public Command<Quiz> StudyCommand => new Command<Quiz>(async (quiz) =>
-        {
-            // Handle study button logic here
-            await DisplayAlert("Study", $"Start studying {quiz.Title}?", "OK");
-        });
+        BindingContext = this;
     }
 
-    // Quiz class
-    public class Quiz
+    // Command for study button
+    public Command<QuizSearch> StudyCommand => new Command<QuizSearch>(async (quiz) =>
     {
-        public string Title { get; set; }
-        public string Creator { get; set; }
-    }
+        // Handle study button logic here
+        await DisplayAlert("Study", $"Start studying {quiz.Title}?", "OK");
+    });
+}
+
+// Quiz class
+public class QuizSearch(string title, string creator) 
+{
+    public string? Title { get; set; } = title;
+    public string? Creator { get; set; } = creator;
 }
