68420c0a522ba9434f4c7f9a39228a9ff1349a4d
jbender3 <benderje26@uwosh.edu>
2024-10-20 18:45:06 -0500

    Add Supabase and initialize it

---
+++ Models/IDatabase.cs
@@ -0,0 +1,5 @@
+﻿namespace QuizzingApp341.Models;
+
+interface IDatabase
+{
+}
---
+++ Models/SupabaseDatabase.cs
@@ -0,0 +1,22 @@
+﻿namespace QuizzingApp341.Models;
+using Supabase;
+class SupabaseDatabase : IDatabase {
+
+    private const string REST_URL = "https://tcogwlqjinvzckjmnjhp.supabase.co";
+    private const string API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjb2d3bHFqa"
+        + "W52emNram1uamhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzNjc4NTIsImV4cCI6MjA0NDk0Mzg1Mn0.bzYJIRYJ3rtvEh3usrydy7M3"
+        + "ES1J6C5iMgPwzlqnTp8";
+
+    private Client Client { get; set; }
+
+    public SupabaseDatabase() {
+        Client = new(REST_URL, API_KEY, new Supabase.SupabaseOptions {
+            AutoConnectRealtime = true // TODO Jeremiah maybe should be false?
+        });
+    }
+
+    public async void Initialize() {
+        // initialize Supabase
+        await Client.InitializeAsync();
+    }
+}
