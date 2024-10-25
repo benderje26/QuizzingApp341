735b5c6705480d1deac579865a89007fa6100da3
Zafeer Rahim <rahimza88@uwosh.edu>
2024-10-25 17:30:44 -0500

    added functionality to the buttons in homescreen

--- Views/HomeScreen.xaml.cs
+++ Views/HomeScreen.xaml.cs
@@ -11,4 +11,28 @@ public partial class HomeScreen : ContentPage
         // Navigate to SearchResultsPage
         Navigation.PushAsync(new Search());
     }
+    private void OnLoginClicked(object sender, EventArgs e)
+    {
+        // Navigate to LoginPage
+        Navigation.PushAsync(new Login());
+    }
+    private void OnCreateClicked(object sender, EventArgs e)
+    {
+        // Navigate to Create account
+        Navigation.PushAsync(new CreateAccount());
+    }
+    private async void OnStartClicked(object sender, EventArgs e)
+    {
+        var quizIdEntry = this.FindByName<Entry>("quizIdEntry");
+        string quizId = quizIdEntry?.Text;
+
+        if (quizId == "12345") 
+        {
+            await Navigation.PushAsync(new TestQuiz("12345")); 
+        }
+        else
+        {
+            await DisplayAlert("Invalid Quiz ID", "Please enter a valid Quiz ID.", "OK");
+        }
+    }
 }
