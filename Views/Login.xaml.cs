c1fc7d8c480e05b1cae1afc41035f5d3dcb76162
Jason Wang <wangj97@uwosh.edu>
2024-10-30 13:28:45 -0500

    add nagigation bar

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -1,5 +1,6 @@
 namespace QuizzingApp341.Views;
-
+using System;
+using Microsoft.Maui.Controls;
 /*
  * Name: Jeremiah Bender
  */
@@ -7,4 +8,17 @@ public partial class Login : ContentPage {
     public Login() {
         InitializeComponent();
     }
+
+    private async void OnLoginButtonClicked(object sender, EventArgs e) {
+        bool loginSuccessful = true; // set true for now, later should check for username and password
+
+        if (loginSuccessful) {
+            // Navigate to HomeScreen and make TabBar visible
+            await Shell.Current.GoToAsync("//HomeScreen");
+            Shell.SetTabBarIsVisible(this, true);
+        } else {
+            await DisplayAlert("Login Failed", "Invalid username or password.", "OK");
+        }
+    }
+
 }
--- Views/QuestionStats.xaml.cs
+++ Views/QuestionStats.xaml.cs
@@ -1,10 +1,46 @@
 namespace QuizzingApp341.Views;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using QuizzingApp341.Models;
+using System.Collections.Generic;
 
 /*
  * Name: Peter Skogman
  */
 public partial class QuestionStats : ContentPage {
-    public QuestionStats() {
+    //private int currentQuestionIndex = 0;
+    //private Quiz currentQuiz;
+
+
+    public QuestionStats(Quiz quiz) {
         InitializeComponent();
+       // currentQuiz = quiz;
     }
-}
+
+    //private async void OnNextQuestionButtonClicked(object sender, EventArgs e) {
+    //    currentQuestionIndex++;
+    //    // Check if there are more questions in the quiz
+    //    if (currentQuestionIndex < currentQuiz.Questions.Count) {
+    //        string question = currentQuiz.Questions[currentQuestionIndex];
+
+
+    //        bool isMultipleChoice = CheckIfMultipleChoice(question);
+
+    //        if (isMultipleChoice) {
+    //            // Navigate to the multiple choice screen
+    //            await Navigation.PushAsync(new MultipleChoice());
+    //        } else {
+    //            // Navigate to the fill-in-the-blank screen
+    //            await Navigation.PushAsync(new FillBlank());
+    //        }
+
+
+    //    } else {
+    //        // No more questions, display statistics page
+    //        await Navigation.PushAsync(new Statistics());
+    //    }
+    }
+
+//    //private bool CheckIfMultipleChoice(string question) {
+//    //    return true;
+//    //}
+//}
