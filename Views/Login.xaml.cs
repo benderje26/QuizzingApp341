34be1cfe0b9db4a319c70a03365c50279e5ac72b
Jason Wang <wangj97@uwosh.edu>
2024-10-30 16:49:36 -0500

    remove functionality on HomeScreen(create acc and login). add nagvation on login screen(resetPassword and create acc). some small layout changes.

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -9,12 +9,22 @@ public partial class Login : ContentPage {
         InitializeComponent();
     }
 
+    private void OnCreateClicked(object sender, EventArgs e) {
+        // Navigate to Create account
+        Navigation.PushAsync(new CreateAccount());
+    }
+
+    private void OnResetPasswordClicked(object sender, EventArgs e) {
+        // Navigate to Create account
+        Navigation.PushAsync(new ResetPassword());
+    }
+
     private async void OnLoginButtonClicked(object sender, EventArgs e) {
         bool loginSuccessful = true; // set true for now, later should check for username and password
 
         if (loginSuccessful) {
             // Navigate to HomeScreen and make TabBar visible
-            await Shell.Current.GoToAsync("//HomeScreen");
+            await Shell.Current.GoToAsync("//HomeScreen");  //TODO: change HomeScreen to UserHome once UserHome got proved 
             Shell.SetTabBarIsVisible(this, true);
         } else {
             await DisplayAlert("Login Failed", "Invalid username or password.", "OK");
