a2afe6bcb90239c3a1f69ce4e6d9e11018864070
leepachia <pachia0lee@gmail.com>
2024-10-30 14:10:49 -0500

    Didn't put in a nav bar yet, but there's some navigation between pages

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -7,4 +7,8 @@ public partial class Login : ContentPage {
     public Login() {
         InitializeComponent();
     }
+
+    private void LoginButtonClicked(object sender, EventArgs e) {
+        Application.Current.MainPage = new NavigationPage(new UserHome()); // Go to user's home page
+    }
 }
---
+++ Views/UserHome.xaml.cs
@@ -0,0 +1,26 @@
+namespace QuizzingApp341.Views;
+public partial class UserHome : ContentPage {
+    public UserHome() {
+        InitializeComponent();
+    }
+
+    private void OnSearchClicked(object sender, EventArgs e) {
+        // Navigate to SearchResultsPage
+        Navigation.PushAsync(new Search());
+    }
+
+    private async void OnStartClicked(object sender, EventArgs e)
+    {
+        var quizIdEntry = this.FindByName<Entry>("quizIdEntry");
+        string quizId = quizIdEntry?.Text;
+
+        if (quizId == "12345") 
+        {
+            await Navigation.PushAsync(new TestQuiz("12345")); 
+        }
+        else
+        {
+            await DisplayAlert("Invalid Quiz ID", "Please enter a valid Quiz ID.", "OK");
+        }
+    }
+}
