54f2b39e24859eee2720faa6a8d9e0999f8a1099
Pachia <161641617+leepachia@users.noreply.github.com>
2024-11-26 09:51:37 -0600

    Made a command to edit a question

--- Views/Create_A_Quiz_Screens/EditQuiz.xaml.cs
+++ Views/Create_A_Quiz_Screens/EditQuiz.xaml.cs
@@ -1,14 +1,21 @@
 namespace QuizzingApp341.Views;
 using QuizzingApp341.Models;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 public partial class EditQuiz : ContentPage {
     public string QuizTitle {get; set;}
     public ObservableCollection<Question> Questions {get; set;}
+    public ICommand QuestionClickedCommand {get; set;}
 
     public EditQuiz(QuizManager quizManager) {
         InitializeComponent();
         QuizTitle = (quizManager.Quiz?.Title != null) ? quizManager.Quiz.Title: "";
         Questions = quizManager.Questions;
+        QuestionClickedCommand = new Command<long>(QuestionClicked);
         BindingContext = this;
+    }
+
+    private async void QuestionClicked(long questionId) {
+        
     }
 }
