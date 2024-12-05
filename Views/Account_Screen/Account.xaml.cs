d3135d3042aee61c3e8233fc9408b7b836b499fe
Peter Skogman <skogmanpe40@uwosh.edu>
2024-12-05 15:45:10 -0600

    Change settings page to account page
    We are removing the settings page and coverting it into the account
    page. Added a new icon for the account page, removed two buttons from
    the page, fixed the bug with the tab bar going away, and renamed the
    files to no longer be settings and be account.

--- Views/Account_Screen/Account.xaml.cs
+++ Views/Account_Screen/Account.xaml.cs
@@ -1,13 +1,12 @@
 namespace QuizzingApp341.Views {
-    public partial class Setting : ContentPage {
-        public Setting() {
+    public partial class Account : ContentPage {
+        public Account() {
             InitializeComponent();
         }
 
         private async void OnSignOutClicked(object sender, EventArgs e) {
             // Navigate back to the Login screen, and hide the TabBar
             await Shell.Current.GoToAsync("//Login", true);
-            Shell.SetTabBarIsVisible(this, false);
         }
 
     }
